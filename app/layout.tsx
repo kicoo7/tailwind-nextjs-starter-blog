6f1d8125ef53d9f8680311a2e209310965d9208c
Krste Rajchevski <rajchevski.krste@live.com>
2024-01-22 18:25:12 +0100

    First commit with Bugpilot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,25 @@
-import 'css/tailwind.css'
-import 'pliny/search/algolia.css'
-
-import { Space_Grotesk } from 'next/font/google'
-import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { SearchProvider, SearchConfig } from 'pliny/search'
-import Header from '@/components/Header'
-import SectionContainer from '@/components/SectionContainer'
-import Footer from '@/components/Footer'
-import siteMetadata from '@/data/siteMetadata'
-import { ThemeProviders } from './theme-providers'
-import { Metadata } from 'next'
-
+import { Bugpilot } from "@bugpilot/plugin-nextjs";
+import 'css/tailwind.css';
+import 'pliny/search/algolia.css';
+import { Space_Grotesk } from 'next/font/google';
+import { Analytics, AnalyticsConfig } from 'pliny/analytics';
+import { SearchProvider, SearchConfig } from 'pliny/search';
+import Header from '@/components/Header';
+import SectionContainer from '@/components/SectionContainer';
+import Footer from '@/components/Footer';
+import siteMetadata from '@/data/siteMetadata';
+import { ThemeProviders } from './theme-providers';
+import { Metadata } from 'next';
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-space-grotesk',
-})
-
+  variable: '--font-space-grotesk'
+});
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
   title: {
     default: siteMetadata.title,
-    template: `%s | ${siteMetadata.title}`,
+    template: `%s | ${siteMetadata.title}`
   },
   description: siteMetadata.description,
   openGraph: {
@@ -31,13 +29,13 @@ export const metadata: Metadata = {
     siteName: siteMetadata.title,
     images: [siteMetadata.socialBanner],
     locale: 'en_US',
-    type: 'website',
+    type: 'website'
   },
   alternates: {
     canonical: './',
     types: {
-      'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
-    },
+      'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`
+    }
   },
   robots: {
     index: true,
@@ -47,23 +45,21 @@ export const metadata: Metadata = {
       follow: true,
       'max-video-preview': -1,
       'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
+      'max-snippet': -1
+    }
   },
   twitter: {
     title: siteMetadata.title,
     card: 'summary_large_image',
-    images: [siteMetadata.socialBanner],
-  },
-}
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html
-      lang={siteMetadata.language}
-      className={`${space_grotesk.variable} scroll-smooth`}
-      suppressHydrationWarning
-    >
+    images: [siteMetadata.socialBanner]
+  }
+};
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return <html lang={siteMetadata.language} className={`${space_grotesk.variable} scroll-smooth`} suppressHydrationWarning>
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
@@ -75,10 +71,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
-          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+          <Analytics analyticsConfig={(siteMetadata.analytics as AnalyticsConfig)} />
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
-              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+              <SearchProvider searchConfig={(siteMetadata.search as SearchConfig)}>
                 <Header />
                 <main className="mb-auto">{children}</main>
               </SearchProvider>
@@ -86,7 +82,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </div>
           </SectionContainer>
         </ThemeProviders>
-      </body>
-    </html>
-  )
+      <Bugpilot workspaceId="71e2aba3-108e-43dd-8459-6b462dc01253" /></body>
+    </html>;
 }
---
+++ bugpilot.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "workspaceId": "71e2aba3-108e-43dd-8459-6b462dc01253",
+  "next": {
+    "productionBrowserSourceMaps": true
+  }
+}
